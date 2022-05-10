eb917343187fd60047ca06dfb4ca5d6f23fea5fc
Ivan Volodin <ivanwolodin@gmail.com>
2022-05-10 16:19:28 +0500

    Add: crush-test and cors processing

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
 const { PORT = 3000 } = process.env;
+
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const { errors, celebrate, Joi } = require('celebrate');
 const helmet = require('helmet');
 
@@ -10,10 +12,13 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { login, createUser } = require('./controllers/auth');
 const { auth } = require('./middlewares/auth');
 const { handler404, generalErrorHandler } = require('./errors/errorHandlers');
-const {REGEX_MAIL_CHECK} = require("./utils/utils");
+const { REGEX_MAIL_CHECK } = require('./utils/utils');
+const { corsAllow } = require('./middlewares/cors');
 
 const app = express();
+
 app.use(helmet());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -23,8 +28,16 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
+app.use(corsAllow);
+
 app.use(requestLogger);
 app.use(errorLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -49,11 +62,8 @@ app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.use(handler404);
-
 app.use(errorLogger);
-
 app.use(errors());
-
 app.use(generalErrorHandler);
 
 app.listen(PORT, () => {
